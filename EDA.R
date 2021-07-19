533aac4107024ede1992459583098344305dc1d1
escaper495 <37874248+escaper495@users.noreply.github.com>
2021-07-19 11:08:06 +0100

    First commit with hourly data

---
+++ EDA.R
@@ -0,0 +1,248 @@
+################### EDA for Hourly Data #######################
+
+# 1.Clare
+
+# 2006 - 2017
+Clare_Ennis_SO2_hourly <- read.csv("AQ data/Hourly/Clare_Ennis_SO2_hourly.csv")
+View(Clare_Ennis_SO2_hourly)
+str(Clare_Ennis_SO2_hourly)
+summary(Clare_Ennis_SO2_hourly)
+
+
+# 2. Cork
+
+# 2016 -2017
+Cork_Bishopstown_ozone_SO2_hourly <- read.csv("AQ data/Hourly/Cork_Bishopstown_ozone_SO2_hourly.csv")
+View(Cork_Bishopstown_ozone_SO2_hourly)
+str(Cork_Bishopstown_ozone_SO2_hourly)
+summary(Cork_Bishopstown_ozone_SO2_hourly)
+
+# 1995 - 2012
+Cork_Glashaboy_ozone_NOx_hourly <- read.csv("AQ data/Hourly/Cork_Glashaboy_ozone_NOx_hourly.csv")
+View(Cork_Glashaboy_ozone_NOx_hourly)
+str(Cork_Glashaboy_ozone_NOx_hourly)
+summary(Cork_Glashaboy_ozone_NOx_hourly)
+
+# 2002 - 2012/13
+Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly <- read.csv("AQ data/Hourly/Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly.csv")
+View(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
+str(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
+summary(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
+
+# 2014 - 2017
+Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly.csv")
+View(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
+str(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
+summary(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
+
+# 3. Dublin
+
+# 2012 - 2014
+Dublin_Balbriggan_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_Balbriggan_CO_NOx_SO2_hourly.csv")
+View(Dublin_Balbriggan_CO_NOx_SO2_hourly)
+str(Dublin_Balbriggan_CO_NOx_SO2_hourly)
+summary(Dublin_Balbriggan_CO_NOx_SO2_hourly)
+
+# 2003 - 2017
+Dublin_Ballyfermot_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_Ballyfermot_NOx_SO2_hourly.csv")
+View(Dublin_Ballyfermot_NOx_SO2_hourly)
+str(Dublin_Ballyfermot_NOx_SO2_hourly)
+summary(Dublin_Ballyfermot_NOx_SO2_hourly)
+
+# 2008 - 2017
+Dublin_Blanchardstown_NOx_hourly <- read.csv("AQ data/Hourly/Dublin_Blanchardstown_NOx_hourly.csv")
+View(Dublin_Blanchardstown_NOx_hourly)
+str(Dublin_Blanchardstown_NOx_hourly)
+summary(Dublin_Blanchardstown_NOx_hourly)
+
+# 2008 - 2016
+Dublin_Clonskeagh_Ozone_hourly <- read.csv("AQ data/Hourly/Dublin_Clonskeagh_Ozone_hourly.csv")
+View(Dublin_Clonskeagh_Ozone_hourly)
+str(Dublin_Clonskeagh_Ozone_hourly)
+summary(Dublin_Clonskeagh_Ozone_hourly)
+
+# 2001 - 2017
+Dublin_ColeraineStreet_CO_NOx_SOx_hourly <- read.csv("AQ data/Hourly/Dublin_ColeraineStreet_CO_NOx_SOx_hourly.csv")
+View(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
+str(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
+summary(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
+
+# 2008 - 2017
+Dublin_DunLaoghaire_NOx_hourly <- read.csv("AQ data/Hourly/Dublin_DunLaoghaire_NOx_hourly.csv")
+View(Dublin_DunLaoghaire_NOx_hourly)
+str(Dublin_DunLaoghaire_NOx_hourly)
+summary(Dublin_DunLaoghaire_NOx_hourly)
+
+# 2007 - 2009
+Dublin_Knocklyon_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_Knocklyon_CO_NOx_SO2_hourly.csv")
+View(Dublin_Knocklyon_CO_NOx_SO2_hourly)
+str(Dublin_Knocklyon_CO_NOx_SO2_hourly)
+summary(Dublin_Knocklyon_CO_NOx_SO2_hourly)
+
+# 1994 - 2001 (2/2/2002)
+Dublin_PotteryRoad_ozone_94_02_hourly <- read.csv("AQ data/Hourly/Dublin_PotteryRoad_ozone_94_02_hourly.csv")
+View(Dublin_PotteryRoad_ozone_94_02_hourly)
+str(Dublin_PotteryRoad_ozone_94_02_hourly)
+summary(Dublin_PotteryRoad_ozone_94_02_hourly)
+
+# 1988 - 2017
+Dublin_Rathmines_NOx_O3_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_Rathmines_NOx_O3_SO2_hourly.csv")
+View(Dublin_Rathmines_NOx_O3_SO2_hourly)
+str(Dublin_Rathmines_NOx_O3_SO2_hourly)
+summary(Dublin_Rathmines_NOx_O3_SO2_hourly)
+
+# 2009 - 2017
+Dublin_Ringsend_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_Ringsend_CO_NOx_SO2_hourly.csv")
+View(Dublin_Ringsend_CO_NOx_SO2_hourly)
+str(Dublin_Ringsend_CO_NOx_SO2_hourly)
+summary(Dublin_Ringsend_CO_NOx_SO2_hourly)
+
+# 2013 - 2015
+Dublin_StAnnesPark_NOx_hourly <- read.csv("AQ data/Hourly/Dublin_StAnnesPark_NOx_hourly.csv")
+View(Dublin_StAnnesPark_NOx_hourly)
+str(Dublin_StAnnesPark_NOx_hourly)
+summary(Dublin_StAnnesPark_NOx_hourly)
+
+# 2010 - 2017
+Dublin_Swords_NOx_Ozone_hourly <- read.csv("AQ data/Hourly/Dublin_Swords_NOx_Ozone_hourly.csv")
+View(Dublin_Swords_NOx_Ozone_hourly)
+str(Dublin_Swords_NOx_Ozone_hourly)
+summary(Dublin_Swords_NOx_Ozone_hourly)
+
+# 2009 -2016
+Dublin_Tallaght_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_Tallaght_SO2_hourly.csv")
+View(Dublin_Tallaght_SO2_hourly)
+str(Dublin_Tallaght_SO2_hourly)
+summary(Dublin_Tallaght_SO2_hourly)
+
+# 2001 - 2017
+Dublin_WinetavernSt_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Dublin_WinetavernSt_CO_NOx_SO2_hourly.csv")
+View(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
+str(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
+summary(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
+
+
+# 4.Galway
+
+# 1994 - 2017
+Galway_MaceHead_ozone_hourly <- read.csv("AQ data/Hourly/Galway_MaceHead_ozone_hourly.csv")
+View(Galway_MaceHead_ozone_hourly)
+str(Galway_MaceHead_ozone_hourly)
+summary(Galway_MaceHead_ozone_hourly)
+
+
+# 5.Kerry
+
+# 2001 - 2017
+Kerry_Valentia_ozone_hourl <- read.csv("AQ data/Hourly/Kerry_Valentia_ozone_hourly.csv")
+View(Kerry_Valentia_ozone_hourl)
+str(Kerry_Valentia_ozone_hourl)
+summary(Kerry_Valentia_ozone_hourl)
+
+
+# 6.Kilkenny
+
+# 2012 - 2017
+Kilkenny_SevilleLodge_NOx_Ozone_hourly <- read.csv("AQ data/Hourly/Kilkenny_SevilleLodge_NOx_Ozone_hourly.csv")
+View(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
+str(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
+summary(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
+
+
+# 7.Laois
+
+# 2004 - 2017
+Laois_EmoCourt_NOx_Ozone_hourly <- read.csv("AQ data/Hourly/Laois_EmoCourt_NOx_Ozone_hourly.csv")
+View(Laois_EmoCourt_NOx_Ozone_hourly)
+str(Laois_EmoCourt_NOx_Ozone_hourly)
+summary(Laois_EmoCourt_NOx_Ozone_hourly)
+
+# 2014 - 2017
+Laois_Portlaoise_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Laois_Portlaoise_CO_NOx_SO2_hourly.csv")
+View(Laois_Portlaoise_CO_NOx_SO2_hourly)
+str(Laois_Portlaoise_CO_NOx_SO2_hourly)
+summary(Laois_Portlaoise_CO_NOx_SO2_hourly)
+
+
+# 8.Limerick
+
+# 2005 - 2011
+Limerick_Parkroad_NOx_Ozone_hourly <- read.csv("AQ data/Hourly/Limerick_Parkroad_NOx_Ozone_hourly.csv")
+View(Limerick_Parkroad_NOx_Ozone_hourly)
+str(Limerick_Parkroad_NOx_Ozone_hourly)
+summary(Limerick_Parkroad_NOx_Ozone_hourly)
+
+# 1999 - 2006
+Limerick_Shannon_SO2_hourly <- read.csv("AQ data/Hourly/Limerick_Shannon_SO2_hourly.csv")
+View(Limerick_Shannon_SO2_hourly)
+str(Limerick_Shannon_SO2_hourly)
+summary(Limerick_Shannon_SO2_hourly)
+
+# 2007 - 2016
+Limerick_ShannonEstuary_SO2_hourly <- read.csv("AQ data/Hourly/Limerick_ShannonEstuary_SO2_hourly.csv")
+View(Limerick_ShannonEstuary_SO2_hourly)
+str(Limerick_ShannonEstuary_SO2_hourly)
+summary(Limerick_ShannonEstuary_SO2_hourly)
+
+
+# 9.Mayo
+
+# 2009 - 2017
+Mayo_Castlebar_NOx_ozone_hourly <- read.csv("AQ data/Hourly/Mayo_Castlebar_NOx_ozone_hourly.csv")
+View(Mayo_Castlebar_NOx_ozone_hourly)
+str(Mayo_Castlebar_NOx_ozone_hourly)
+summary(Mayo_Castlebar_NOx_ozone_hourly)
+
+
+# 10. Monaghan
+
+# 1997 - 2017
+Monaghan_Kilkitt_NOx_ozone_SO2_hourly <- read.csv("AQ data/Hourly/Monaghan_Kilkitt_NOx_ozone_SO2_hourly.csv")
+View(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
+str(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
+summary(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
+
+# 1995 - 2006
+Monaghan_Monaghan_NOx_ozone_SO2_hourly <- read.csv("AQ data/Hourly/Monaghan_Monaghan_NOx_ozone_SO2_hourly.csv")
+View(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
+str(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
+summary(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
+
+
+# 11.Westmeath
+
+# 2012 - 2014
+Westmeath_Mullingar_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Westmeath_Mullingar_CO_NOx_SO2_hourly.csv")
+View(Westmeath_Mullingar_CO_NOx_SO2_hourly)
+str(Westmeath_Mullingar_CO_NOx_SO2_hourly)
+summary(Westmeath_Mullingar_CO_NOx_SO2_hourly)
+
+
+# 12.Wexford
+
+# 2014 -2016
+Wexford_Enniscorthy_CO_NOx_SO2_hourly <- read.csv("AQ data/Hourly/Wexford_Enniscorthy_CO_NOx_SO2_hourly.csv")
+View(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
+str(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
+summary(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
+
+# 1999 - 2009
+Wexford_JohnstoneCastle_ozone_hourly <- read.csv("AQ data/Hourly/Wexford_JohnstoneCastle_ozone_hourly.csv")
+View(Wexford_JohnstoneCastle_ozone_hourly)
+str(Wexford_JohnstoneCastle_ozone_hourly)
+summary(Wexford_JohnstoneCastle_ozone_hourly)
+
+
+# 13. Wicklow
+
+# 1995 - 2000
+Wicklow_Avondale_ozone_hourly <- read.csv("AQ data/Hourly/Wicklow_Avondale_ozone_hourly.csv")
+View(Wicklow_Avondale_ozone_hourly)
+str(Wicklow_Avondale_ozone_hourly)
+summary(Wicklow_Avondale_ozone_hourly)
+
+# 2010 - 2017
+Wicklow_Bray_ozone_hourly <- read.csv("AQ data/Hourly/Wicklow_Bray_ozone_hourly.csv")
+View(Wicklow_Bray_ozone_hourly)
+str(Wicklow_Bray_ozone_hourly)
+summary(Wicklow_Bray_ozone_hourly)
