877c36a84c2cf84c4e7bf7ed2c2d7c65b57ba842
escaper495 <vedant143bhagwat@gmail.com>
2021-08-02 14:11:05 +0100

    missing data calculation for all air pollutants

--- EDA.R
+++ EDA.R
@@ -4,7 +4,7 @@
 
 # 2006 - 2017
 Clare_Ennis_SO2_hourly <- read.csv("../AQ data/Hourly/Clare_Ennis_SO2_hourly.csv")
-View(Clare_Ennis_SO2_hourly)
+# View(Clare_Ennis_SO2_hourly)
 str(Clare_Ennis_SO2_hourly)
 summary(Clare_Ennis_SO2_hourly)
 
@@ -13,25 +13,25 @@ summary(Clare_Ennis_SO2_hourly)
 
 # 2016 -2017
 Cork_Bishopstown_ozone_SO2_hourly <- read.csv("../AQ data/Hourly/Cork_Bishopstown_ozone_SO2_hourly.csv")
-View(Cork_Bishopstown_ozone_SO2_hourly)
+# View(Cork_Bishopstown_ozone_SO2_hourly)
 str(Cork_Bishopstown_ozone_SO2_hourly)
 summary(Cork_Bishopstown_ozone_SO2_hourly)
 
 # 1995 - 2012
 Cork_Glashaboy_ozone_NOx_hourly <- read.csv("../AQ data/Hourly/Cork_Glashaboy_ozone_NOx_hourly.csv")
-View(Cork_Glashaboy_ozone_NOx_hourly)
+# View(Cork_Glashaboy_ozone_NOx_hourly)
 str(Cork_Glashaboy_ozone_NOx_hourly)
 summary(Cork_Glashaboy_ozone_NOx_hourly)
 
 # 2002 - 2012/13
 Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly <- read.csv("../AQ data/Hourly/Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly.csv")
-View(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
+# View(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
 str(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
 summary(Cork_OldStationRoad_CO_NOx_ozone_SO2_hourly)
 
 # 2014 - 2017
 Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly.csv")
-View(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
+# View(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
 str(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
 summary(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
 
@@ -39,85 +39,85 @@ summary(Cork_SouthLinkRoad_CO_ozone_NOx_SO2_hourly)
 
 # 2012 - 2014
 Dublin_Balbriggan_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_Balbriggan_CO_NOx_SO2_hourly.csv")
-View(Dublin_Balbriggan_CO_NOx_SO2_hourly)
+# View(Dublin_Balbriggan_CO_NOx_SO2_hourly)
 str(Dublin_Balbriggan_CO_NOx_SO2_hourly)
 summary(Dublin_Balbriggan_CO_NOx_SO2_hourly)
 
 # 2003 - 2017
 Dublin_Ballyfermot_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_Ballyfermot_NOx_SO2_hourly.csv")
-View(Dublin_Ballyfermot_NOx_SO2_hourly)
+# View(Dublin_Ballyfermot_NOx_SO2_hourly)
 str(Dublin_Ballyfermot_NOx_SO2_hourly)
 summary(Dublin_Ballyfermot_NOx_SO2_hourly)
 
 # 2008 - 2017
 Dublin_Blanchardstown_NOx_hourly <- read.csv("../AQ data/Hourly/Dublin_Blanchardstown_NOx_hourly.csv")
-View(Dublin_Blanchardstown_NOx_hourly)
+# View(Dublin_Blanchardstown_NOx_hourly)
 str(Dublin_Blanchardstown_NOx_hourly)
 summary(Dublin_Blanchardstown_NOx_hourly)
 
 # 2008 - 2016
 Dublin_Clonskeagh_Ozone_hourly <- read.csv("../AQ data/Hourly/Dublin_Clonskeagh_Ozone_hourly.csv")
-View(Dublin_Clonskeagh_Ozone_hourly)
+# View(Dublin_Clonskeagh_Ozone_hourly)
 str(Dublin_Clonskeagh_Ozone_hourly)
 summary(Dublin_Clonskeagh_Ozone_hourly)
 
 # 2001 - 2017
 Dublin_ColeraineStreet_CO_NOx_SOx_hourly <- read.csv("../AQ data/Hourly/Dublin_ColeraineStreet_CO_NOx_SOx_hourly.csv")
-View(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
+# View(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
 str(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
 summary(Dublin_ColeraineStreet_CO_NOx_SOx_hourly)
 
 # 2008 - 2017
 Dublin_DunLaoghaire_NOx_hourly <- read.csv("../AQ data/Hourly/Dublin_DunLaoghaire_NOx_hourly.csv")
-View(Dublin_DunLaoghaire_NOx_hourly)
+# View(Dublin_DunLaoghaire_NOx_hourly)
 str(Dublin_DunLaoghaire_NOx_hourly)
 summary(Dublin_DunLaoghaire_NOx_hourly)
 
 # 2007 - 2009
 Dublin_Knocklyon_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_Knocklyon_CO_NOx_SO2_hourly.csv")
-View(Dublin_Knocklyon_CO_NOx_SO2_hourly)
+# View(Dublin_Knocklyon_CO_NOx_SO2_hourly)
 str(Dublin_Knocklyon_CO_NOx_SO2_hourly)
 summary(Dublin_Knocklyon_CO_NOx_SO2_hourly)
 
 # 1994 - 2001 (2/2/2002)
 Dublin_PotteryRoad_ozone_94_02_hourly <- read.csv("../AQ data/Hourly/Dublin_PotteryRoad_ozone_94_02_hourly.csv")
-View(Dublin_PotteryRoad_ozone_94_02_hourly)
+# View(Dublin_PotteryRoad_ozone_94_02_hourly)
 str(Dublin_PotteryRoad_ozone_94_02_hourly)
 summary(Dublin_PotteryRoad_ozone_94_02_hourly)
 
 # 1988 - 2017
 Dublin_Rathmines_NOx_O3_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_Rathmines_NOx_O3_SO2_hourly.csv")
-View(Dublin_Rathmines_NOx_O3_SO2_hourly)
+# View(Dublin_Rathmines_NOx_O3_SO2_hourly)
 str(Dublin_Rathmines_NOx_O3_SO2_hourly)
 summary(Dublin_Rathmines_NOx_O3_SO2_hourly)
 
 # 2009 - 2017
 Dublin_Ringsend_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_Ringsend_CO_NOx_SO2_hourly.csv")
-View(Dublin_Ringsend_CO_NOx_SO2_hourly)
+# View(Dublin_Ringsend_CO_NOx_SO2_hourly)
 str(Dublin_Ringsend_CO_NOx_SO2_hourly)
 summary(Dublin_Ringsend_CO_NOx_SO2_hourly)
 
 # 2013 - 2015
 Dublin_StAnnesPark_NOx_hourly <- read.csv("../AQ data/Hourly/Dublin_StAnnesPark_NOx_hourly.csv")
-View(Dublin_StAnnesPark_NOx_hourly)
+# View(Dublin_StAnnesPark_NOx_hourly)
 str(Dublin_StAnnesPark_NOx_hourly)
 summary(Dublin_StAnnesPark_NOx_hourly)
 
 # 2010 - 2017
 Dublin_Swords_NOx_Ozone_hourly <- read.csv("../AQ data/Hourly/Dublin_Swords_NOx_Ozone_hourly.csv")
-View(Dublin_Swords_NOx_Ozone_hourly)
+# View(Dublin_Swords_NOx_Ozone_hourly)
 str(Dublin_Swords_NOx_Ozone_hourly)
 summary(Dublin_Swords_NOx_Ozone_hourly)
 
 # 2009 -2016
 Dublin_Tallaght_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_Tallaght_SO2_hourly.csv")
-View(Dublin_Tallaght_SO2_hourly)
+# View(Dublin_Tallaght_SO2_hourly)
 str(Dublin_Tallaght_SO2_hourly)
 summary(Dublin_Tallaght_SO2_hourly)
 
 # 2001 - 2017
 Dublin_WinetavernSt_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Dublin_WinetavernSt_CO_NOx_SO2_hourly.csv")
-View(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
+# View(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
 str(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
 summary(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
 
@@ -126,7 +126,7 @@ summary(Dublin_WinetavernSt_CO_NOx_SO2_hourly)
 
 # 1994 - 2017
 Galway_MaceHead_ozone_hourly <- read.csv("../AQ data/Hourly/Galway_MaceHead_ozone_hourly.csv")
-View(Galway_MaceHead_ozone_hourly)
+# View(Galway_MaceHead_ozone_hourly)
 str(Galway_MaceHead_ozone_hourly)
 summary(Galway_MaceHead_ozone_hourly)
 
@@ -135,7 +135,7 @@ summary(Galway_MaceHead_ozone_hourly)
 
 # 2001 - 2017
 Kerry_Valentia_ozone_hourl <- read.csv("../AQ data/Hourly/Kerry_Valentia_ozone_hourly.csv")
-View(Kerry_Valentia_ozone_hourl)
+# View(Kerry_Valentia_ozone_hourl)
 str(Kerry_Valentia_ozone_hourl)
 summary(Kerry_Valentia_ozone_hourl)
 
@@ -144,7 +144,7 @@ summary(Kerry_Valentia_ozone_hourl)
 
 # 2012 - 2017
 Kilkenny_SevilleLodge_NOx_Ozone_hourly <- read.csv("../AQ data/Hourly/Kilkenny_SevilleLodge_NOx_Ozone_hourly.csv")
-View(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
+# View(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
 str(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
 summary(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
 
@@ -153,13 +153,13 @@ summary(Kilkenny_SevilleLodge_NOx_Ozone_hourly)
 
 # 2004 - 2017
 Laois_EmoCourt_NOx_Ozone_hourly <- read.csv("../AQ data/Hourly/Laois_EmoCourt_NOx_Ozone_hourly.csv")
-View(Laois_EmoCourt_NOx_Ozone_hourly)
+# View(Laois_EmoCourt_NOx_Ozone_hourly)
 str(Laois_EmoCourt_NOx_Ozone_hourly)
 summary(Laois_EmoCourt_NOx_Ozone_hourly)
 
 # 2014 - 2017
 Laois_Portlaoise_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Laois_Portlaoise_CO_NOx_SO2_hourly.csv")
-View(Laois_Portlaoise_CO_NOx_SO2_hourly)
+# View(Laois_Portlaoise_CO_NOx_SO2_hourly)
 str(Laois_Portlaoise_CO_NOx_SO2_hourly)
 summary(Laois_Portlaoise_CO_NOx_SO2_hourly)
 
@@ -168,19 +168,19 @@ summary(Laois_Portlaoise_CO_NOx_SO2_hourly)
 
 # 2005 - 2011
 Limerick_Parkroad_NOx_Ozone_hourly <- read.csv("../AQ data/Hourly/Limerick_Parkroad_NOx_Ozone_hourly.csv")
-View(Limerick_Parkroad_NOx_Ozone_hourly)
+# View(Limerick_Parkroad_NOx_Ozone_hourly)
 str(Limerick_Parkroad_NOx_Ozone_hourly)
 summary(Limerick_Parkroad_NOx_Ozone_hourly)
 
 # 1999 - 2006
 Limerick_Shannon_SO2_hourly <- read.csv("../AQ data/Hourly/Limerick_Shannon_SO2_hourly.csv")
-View(Limerick_Shannon_SO2_hourly)
+# View(Limerick_Shannon_SO2_hourly)
 str(Limerick_Shannon_SO2_hourly)
 summary(Limerick_Shannon_SO2_hourly)
 
 # 2007 - 2016
 Limerick_ShannonEstuary_SO2_hourly <- read.csv("../AQ data/Hourly/Limerick_ShannonEstuary_SO2_hourly.csv")
-View(Limerick_ShannonEstuary_SO2_hourly)
+# View(Limerick_ShannonEstuary_SO2_hourly)
 str(Limerick_ShannonEstuary_SO2_hourly)
 summary(Limerick_ShannonEstuary_SO2_hourly)
 
@@ -189,7 +189,7 @@ summary(Limerick_ShannonEstuary_SO2_hourly)
 
 # 2009 - 2017
 Mayo_Castlebar_NOx_ozone_hourly <- read.csv("../AQ data/Hourly/Mayo_Castlebar_NOx_ozone_hourly.csv")
-View(Mayo_Castlebar_NOx_ozone_hourly)
+# View(Mayo_Castlebar_NOx_ozone_hourly)
 str(Mayo_Castlebar_NOx_ozone_hourly)
 summary(Mayo_Castlebar_NOx_ozone_hourly)
 
@@ -198,13 +198,13 @@ summary(Mayo_Castlebar_NOx_ozone_hourly)
 
 # 1997 - 2017
 Monaghan_Kilkitt_NOx_ozone_SO2_hourly <- read.csv("../AQ data/Hourly/Monaghan_Kilkitt_NOx_ozone_SO2_hourly.csv")
-View(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
+# View(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
 str(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
 summary(Monaghan_Kilkitt_NOx_ozone_SO2_hourly)
 
 # 1995 - 2006
 Monaghan_Monaghan_NOx_ozone_SO2_hourly <- read.csv("../AQ data/Hourly/Monaghan_Monaghan_NOx_ozone_SO2_hourly.csv")
-View(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
+# View(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
 str(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
 summary(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
 
@@ -213,7 +213,7 @@ summary(Monaghan_Monaghan_NOx_ozone_SO2_hourly)
 
 # 2012 - 2014
 Westmeath_Mullingar_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Westmeath_Mullingar_CO_NOx_SO2_hourly.csv")
-View(Westmeath_Mullingar_CO_NOx_SO2_hourly)
+# View(Westmeath_Mullingar_CO_NOx_SO2_hourly)
 str(Westmeath_Mullingar_CO_NOx_SO2_hourly)
 summary(Westmeath_Mullingar_CO_NOx_SO2_hourly)
 
@@ -222,13 +222,13 @@ summary(Westmeath_Mullingar_CO_NOx_SO2_hourly)
 
 # 2014 -2016
 Wexford_Enniscorthy_CO_NOx_SO2_hourly <- read.csv("../AQ data/Hourly/Wexford_Enniscorthy_CO_NOx_SO2_hourly.csv")
-View(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
+# View(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
 str(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
 summary(Wexford_Enniscorthy_CO_NOx_SO2_hourly)
 
 # 1999 - 2009
 Wexford_JohnstoneCastle_ozone_hourly <- read.csv("../AQ data/Hourly/Wexford_JohnstoneCastle_ozone_hourly.csv")
-View(Wexford_JohnstoneCastle_ozone_hourly)
+# View(Wexford_JohnstoneCastle_ozone_hourly)
 str(Wexford_JohnstoneCastle_ozone_hourly)
 summary(Wexford_JohnstoneCastle_ozone_hourly)
 
@@ -237,13 +237,13 @@ summary(Wexford_JohnstoneCastle_ozone_hourly)
 
 # 1995 - 2000
 Wicklow_Avondale_ozone_hourly <- read.csv("../AQ data/Hourly/Wicklow_Avondale_ozone_hourly.csv")
-View(Wicklow_Avondale_ozone_hourly)
+# View(Wicklow_Avondale_ozone_hourly)
 str(Wicklow_Avondale_ozone_hourly)
 summary(Wicklow_Avondale_ozone_hourly)
 
 # 2010 - 2017
 Wicklow_Bray_ozone_hourly <- read.csv("../AQ data/Hourly/Wicklow_Bray_ozone_hourly.csv")
-View(Wicklow_Bray_ozone_hourly)
+# View(Wicklow_Bray_ozone_hourly)
 str(Wicklow_Bray_ozone_hourly)
 summary(Wicklow_Bray_ozone_hourly)
 
@@ -252,7 +252,7 @@ summary(Wicklow_Bray_ozone_hourly)
 # Choose the year for which data should be taken into consideration.
 hourly_data <- read.csv("Hourly data.csv")
 attach(hourly_data)
-View(hourly_data)
+# View(hourly_data)
 
 str(hourly_data)
 
@@ -265,10 +265,16 @@ hourly_data$CO <- factor(CO)
 hourly_data$NOx <- factor(NOx)
 hourly_data$Ozone <- factor(Ozone)
 
+attach(hourly_data)
 summary(hourly_data)
 
 hist(Start)
 hist(Finish)
+
+# Get hourly data with year 2017
+data_2017 <- hourly_data[hourly_data$Finish==2017 && hourly_data$Start < 2014,]
+nrow(data_2017) # 35
+# subset(hourly_data,Finish==2017)
 
 library(ggplot2)
 ggplot(hourly_data, aes(x=Finish))+ geom_histogram() +
@@ -286,57 +292,59 @@ ggplot(hourly_data, aes(x=Finish))+ geom_histogram() +
 library(readxl)
 X2018_Hourly_NO_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2018 Hourly NO for UCC.xls", 
                                       skip = 2)
-View(X2018_Hourly_NO_for_UCC)
+# View(X2018_Hourly_NO_for_UCC)
 X2018_Hourly_NO_for_UCC_stats <- X2018_Hourly_NO_for_UCC[c(8764:8771),]
 X2018_Hourly_NO_for_UCC <- X2018_Hourly_NO_for_UCC[-c(1,2,(8764:8771)),]
+str(X2018_Hourly_NO_for_UCC)
+summary(X2018_Hourly_NO_for_UCC)
 
 X2019_Hourly_NO_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2019 Hourly NO for UCC.xls", 
                                       skip = 2)
-View(X2019_Hourly_NO_for_UCC)
+# View(X2019_Hourly_NO_for_UCC)
 X2019_Hourly_NO_for_UCC_stats <- X2019_Hourly_NO_for_UCC[c(8764:8771),]
 X2019_Hourly_NO_for_UCC <- X2019_Hourly_NO_for_UCC[-c(1,2,(8764:8771)),]
 
 X2020_Hourly_NO_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2020 updated NO for UCC.xls", 
                                        skip = 2)
-View(X2020_Hourly_NO_for_UCC)
+# View(X2020_Hourly_NO_for_UCC)
 X2020_Hourly_NO_for_UCC_stats <- X2020_Hourly_NO_for_UCC[c(4290:4297),]
 X2020_Hourly_NO_for_UCC <- X2020_Hourly_NO_for_UCC[-c(1,2,(4290:4297)),]
 
 # NO2 
 X2018_Hourly_NO2_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2018 Hourly NO2 for UCC.xls", 
                                       skip = 2)
-View(X2018_Hourly_NO2_for_UCC)
+# View(X2018_Hourly_NO2_for_UCC)
 X2018_Hourly_NO2_for_UCC_stats <- X2018_Hourly_NO2_for_UCC[c(8764:8771),]
 X2018_Hourly_NO2_for_UCC <- X2018_Hourly_NO2_for_UCC[-c(1,2,(8764:8771)),]
 
 X2019_Hourly_NO2_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2019 Hourly NO2 for UCC.xls", 
                                       skip = 2)
-View(X2019_Hourly_NO2_for_UCC)
+# View(X2019_Hourly_NO2_for_UCC)
 X2019_Hourly_NO2_for_UCC_stats <- X2019_Hourly_NO2_for_UCC[c(8764:8771),]
 X2019_Hourly_NO2_for_UCC <- X2019_Hourly_NO2_for_UCC[-c(1,2,(8764:8771)),]
 
 X2020_Hourly_NO2_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2020 updated  NO2 for UCC.xls", 
                                        skip = 2)
-View(X2020_Hourly_NO2_for_UCC)
+# View(X2020_Hourly_NO2_for_UCC)
 X2020_Hourly_NO2_for_UCC_stats <- X2020_Hourly_NO2_for_UCC[c(4290:4297),]
 X2020_Hourly_NO2_for_UCC <- X2020_Hourly_NO2_for_UCC[-c(1,2,(4290:4297)),]
 
 # PM 2.5
 X2018_Hourly_PM2_5_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2018 Hourly PM2.5 for UCC.xls", 
                                          skip = 2)
-View(X2018_Hourly_PM2_5_for_UCC)
+# View(X2018_Hourly_PM2_5_for_UCC)
 X2018_Hourly_PM2_5_for_UCC_stats <- X2018_Hourly_PM2_5_for_UCC[c(8764:8771),]
 X2018_Hourly_PM2_5_for_UCC <- X2018_Hourly_PM2_5_for_UCC[-c(1,2,(8764:8771)),]
 
 X2019_Hourly_PM2_5_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2019 Hourly PM2.5 for UCC.xls", 
                                          skip = 2)
-View(X2019_Hourly_PM2_5_for_UCC)
+# View(X2019_Hourly_PM2_5_for_UCC)
 X2019_Hourly_PM2_5_for_UCC_stats <- X2019_Hourly_PM2_5_for_UCC[c(8764:8771),]
 X2019_Hourly_PM2_5_for_UCC <- X2019_Hourly_PM2_5_for_UCC[-c(1,2,(8764:8771)),]
 
 X2020_Hourly_PM2_5_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2020 updated PM25 for UCC.xls", 
                                          skip = 2)
-View(X2020_Hourly_PM2_5_for_UCC)
+# View(X2020_Hourly_PM2_5_for_UCC)
 X2020_Hourly_PM2_5_for_UCC_stats <- X2020_Hourly_PM2_5_for_UCC[c(4290:4297),]
 X2020_Hourly_PM2_5_for_UCC <- X2020_Hourly_PM2_5_for_UCC[-c(1,2,(4290:4297)),]
 
@@ -344,39 +352,39 @@ X2020_Hourly_PM2_5_for_UCC <- X2020_Hourly_PM2_5_for_UCC[-c(1,2,(4290:4297)),]
 # PM 10
 X2018_Hourly_PM10_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2018 Hourly PM10 for UCC.xls", 
                                         skip = 2)
-View(X2018_Hourly_PM10_for_UCC)
+# View(X2018_Hourly_PM10_for_UCC)
 X2018_Hourly_PM10_for_UCC_stats <- X2018_Hourly_PM10_for_UCC[c(8764:8771),]
 X2018_Hourly_PM10_for_UCC <- X2018_Hourly_PM10_for_UCC[-c(1,2,(8764:8771)),]
 
 X2019_Hourly_PM10_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2019 Hourly PM10 for UCC.xls", 
                                         skip = 2)
-View(X2019_Hourly_PM10_for_UCC)
+# View(X2019_Hourly_PM10_for_UCC)
 X2019_Hourly_PM10_for_UCC_stats <- X2019_Hourly_PM10_for_UCC[c(8764:8771),]
 X2019_Hourly_PM10_for_UCC <- X2019_Hourly_PM10_for_UCC[-c(1,2,(8764:8771)),]
 
 
 X2020_Hourly_PM10_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2020 updated PM10 for UCC.xls", 
                                         skip = 2)
-View(X2020_Hourly_PM10_for_UCC)
+# View(X2020_Hourly_PM10_for_UCC)
 X2020_Hourly_PM10_for_UCC_stats <- X2020_Hourly_PM10_for_UCC[c(4290:4297),]
 X2020_Hourly_PM10_for_UCC <- X2020_Hourly_PM10_for_UCC[-c(1,2,(4290:4297)),]
 
 # Ozone
 X2018_Hourly_O3_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2018 Hourly O3 for UCC.xls", 
                                       skip = 2)
-View(X2018_Hourly_O3_for_UCC)
+# View(X2018_Hourly_O3_for_UCC)
 X2018_Hourly_O3_for_UCC_stats <- X2018_Hourly_O3_for_UCC[c(8764:8771),]
 X2018_Hourly_O3_for_UCC <- X2018_Hourly_O3_for_UCC[-c(1,2,(8764:8771)),]
 
 X2019_Hourly_O3_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2019 Hourly O3 for UCC.xls", 
                                       skip = 2)
-View(X2019_Hourly_O3_for_UCC)
+# View(X2019_Hourly_O3_for_UCC)
 X2019_Hourly_O3_for_UCC_stats <- X2019_Hourly_O3_for_UCC[c(8764:8771),]
 X2019_Hourly_O3_for_UCC <- X2019_Hourly_O3_for_UCC[-c(1,2,(8764:8771)),]
 
 X2020_Hourly_O3_for_UCC <- read_excel("E:/CIT/Project/AQ data/AAMP/2020 updated Ozone for UCC.xls", 
                                       skip = 2)
-View(X2020_Hourly_O3_for_UCC)
+# View(X2020_Hourly_O3_for_UCC)
 X2020_Hourly_O3_for_UCC_stats <- X2020_Hourly_O3_for_UCC[c(4290:4297),]
 X2020_Hourly_O3_for_UCC <- X2020_Hourly_O3_for_UCC[-c(1,2,(4290:4297)),]
 
@@ -417,13 +425,13 @@ str(X2020_Hourly_O3_for_UCC)
 # install.packages("xts")
 # library(xts)
 # train_xts <- xts(x = X2020_Hourly_O3_for_UCC, order.by = X2020_Hourly_O3_for_UCC$`Date & Time`)
-# View(train_xts)
+# # View(train_xts)
 
 plot.ts(X2020_Hourly_O3_for_UCC$Bray)
 plot.ts(X2020_Hourly_O3_for_UCC$Castlebar)
 
 timeseries <- ts(X2020_Hourly_O3_for_UCC$Bray, frequency=12, start=c(2020,1))
-View(timeseries)
+# View(timeseries)
 plot.ts(timeseries)
 
 
---
+++ missing data PM10.R
@@ -0,0 +1,105 @@
+########### 2018 PM10
+
+str(X2018_Hourly_PM10_for_UCC)
+
+typeof(X2018_Hourly_PM10_for_UCC)
+# Convert data into data frame
+X2018_Hourly_PM10_for_UCC <- data.frame(X2018_Hourly_PM10_for_UCC)
+
+
+# Convert string date to Date object
+colnames(X2018_Hourly_PM10_for_UCC)[1] <- "Date"
+X2018_Hourly_PM10_for_UCC$Date <- as.Date(X2018_Hourly_PM10_for_UCC$Date)
+
+# Convert character numbers into double and replace NA values with mean
+for(i in 2:ncol(X2018_Hourly_PM10_for_UCC)){
+  X2018_Hourly_PM10_for_UCC[,i] <- as.double(X2018_Hourly_PM10_for_UCC[,i])
+  # X2018_Hourly_PM10_for_UCC[is.na(X2018_Hourly_PM10_for_UCC[,i]), i] <- mean(X2018_Hourly_PM10_for_UCC[,i], na.rm = TRUE)
+}
+
+# X2018_Hourly_PM10_for_UCC$Castlebar <- as.double(X2018_Hourly_PM10_for_UCC$Castlebar)
+
+# Check NA values
+missing_data_PM10_2018 <- data.frame("Station"=colnames(X2018_Hourly_PM10_for_UCC[,-1]), "value"=colSums(is.na(X2018_Hourly_PM10_for_UCC[,-1])))
+md_PM10_2018<-ggplot(data=missing_data_PM10_2018, aes(x=Station, y=value)) +
+  geom_bar(stat="identity", fill="steelblue")
+md_PM10_2018 + 
+  # geom_text(aes(label=value), vjust=1.6, color="white", size=3.5)+
+  geom_text(aes(label=value), vjust=-0.3, size=3.5)+
+  theme_minimal() +
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  ggtitle("Missing data of PM10 in 2018")
+
+str(X2018_Hourly_PM10_for_UCC)
+
+
+
+########### 2019 PM10
+
+str(X2019_Hourly_PM10_for_UCC)
+
+typeof(X2019_Hourly_PM10_for_UCC)
+# Convert data into data frame
+X2019_Hourly_PM10_for_UCC <- data.frame(X2019_Hourly_PM10_for_UCC)
+
+
+# Convert string date to Date object
+colnames(X2019_Hourly_PM10_for_UCC)[1] <- "Date"
+X2019_Hourly_PM10_for_UCC$Date <- as.Date(X2019_Hourly_PM10_for_UCC$Date)
+
+# Convert character numbers into double and replace NA values with mean
+for(i in 2:ncol(X2019_Hourly_PM10_for_UCC)){
+  X2019_Hourly_PM10_for_UCC[,i] <- as.double(X2019_Hourly_PM10_for_UCC[,i])
+  # X2019_Hourly_PM10_for_UCC[is.na(X2019_Hourly_PM10_for_UCC[,i]), i] <- mean(X2019_Hourly_PM10_for_UCC[,i], na.rm = TRUE)
+}
+
+# X2019_Hourly_PM10_for_UCC$Castlebar <- as.double(X2019_Hourly_PM10_for_UCC$Castlebar)
+
+# Check NA values
+missing_data_PM10_2019 <- data.frame("Station"=colnames(X2019_Hourly_PM10_for_UCC[,-1]), "value"=colSums(is.na(X2019_Hourly_PM10_for_UCC[,-1])))
+md_PM10_2019<-ggplot(data=missing_data_PM10_2019, aes(x=Station, y=value)) +
+  geom_bar(stat="identity", fill="steelblue")
+md_PM10_2019 + 
+  # geom_text(aes(label=value), vjust=1.6, color="white", size=3.5)+
+  geom_text(aes(label=value), vjust=-0.3, size=3.5)+
+  theme_minimal() +
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  ggtitle("Missing data of PM10 in 2019")
+
+str(X2019_Hourly_PM10_for_UCC)
+
+
+########### 2020 PM10
+
+str(X2020_Hourly_PM10_for_UCC)
+
+typeof(X2020_Hourly_PM10_for_UCC)
+# Convert data into data frame
+X2020_Hourly_PM10_for_UCC <- data.frame(X2020_Hourly_PM10_for_UCC)
+
+
+# Convert string date to Date object
+colnames(X2020_Hourly_PM10_for_UCC)[1] <- "Date"
+X2020_Hourly_PM10_for_UCC$Date <- as.Date(X2020_Hourly_PM10_for_UCC$Date)
+
+# Convert character numbers into double and replace NA values with mean
+for(i in 2:ncol(X2020_Hourly_PM10_for_UCC)){
+  X2020_Hourly_PM10_for_UCC[,i] <- as.double(X2020_Hourly_PM10_for_UCC[,i])
+  # X2020_Hourly_PM10_for_UCC[is.na(X2020_Hourly_PM10_for_UCC[,i]), i] <- mean(X2020_Hourly_PM10_for_UCC[,i], na.rm = TRUE)
+}
+
+# X2020_Hourly_PM10_for_UCC$Castlebar <- as.double(X2020_Hourly_PM10_for_UCC$Castlebar)
+
+# Check NA values
+missing_data_PM10_2020 <- data.frame("Station"=colnames(X2020_Hourly_PM10_for_UCC[,-1]), "value"=colSums(is.na(X2020_Hourly_PM10_for_UCC[,-1])))
+md_PM10_2020<-ggplot(data=missing_data_PM10_2020, aes(x=Station, y=value)) +
+  geom_bar(stat="identity", fill="steelblue")
+md_PM10_2020 + 
+  # geom_text(aes(label=value), vjust=1.6, color="white", size=3.5)+
+  geom_text(aes(label=value), vjust=-0.3, size=3.5)+
+  theme_minimal() +
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  ggtitle("Missing data of PM10 in 2020")
+
+str(X2020_Hourly_PM10_for_UCC)
+
